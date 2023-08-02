ca4a3ac52ff406535e7b593e0b9680b06f30f9a8
getlikeman <misternikir@gmail.com>
2023-08-02 17:56:59 +0600

    Бейба конфиг не трогай!!

--- config.py
+++ config.py
@@ -3,16 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = "6191542874:AAHhyLFdTIuTanmt-jkINhRrVLiNkaXU8xw"
-# TOKEN = os.getenv('BOT_TOKEN')
-# host = os.getenv('DB_HOST')
-# user = os.getenv('DB_USER')
-# password = os.getenv('DB_PASSWORD')
-# db_name = os.getenv('DB_NAME')
-# port = os.getenv('DB_PORT')
+TOKEN = os.getenv('BOT_TOKEN')
+host = os.getenv('DB_HOST')
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASSWORD')
+db_name = os.getenv('DB_NAME')
+port = os.getenv('DB_PORT')
 PAYMENTS_PROVIDER_TOKEN = os.getenv('PAYMENTS_PROVIDER_TOKEN')
-
-host = "localhost"
-user = "root"
-password = "bebepassword"
-db_name = "mysql"
-port = "3306"
