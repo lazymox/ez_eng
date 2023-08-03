1aa6a17803ccffe6092c6cdf810897e0bcafbe5f
lazymox <135596987+lazymox@users.noreply.github.com>
2023-08-03 12:32:56 +0600

    ОЙ ОЙ ОЙ ОЙ ОЙ ОЙ ОЙ ОЙ

--- functions.py
+++ functions.py
@@ -23,13 +23,6 @@ cd = CallbackData("km", "question", "answer")
 
 # отправка видео
 async def video_send(link, user_id):
-    # video_to_send = YouTube(link)
-    # stream = video_to_send.streams.filter(progressive=True, file_extension='mp4')
-    # stream.get_highest_resolution().download(f'{user_id}', f'{user_id}_{video_to_send.title}')
-    # with open(f'{user_id}/{user_id}_{video_to_send.title}', 'rb') as video_file:
-    #     await bot.send_video(chat_id=user_id, video=video_file,
-    #                          caption=video_to_send.title)
-    # os.remove(f'{user_id}/{user_id}_{video_to_send.title}')
     await bot.send_message(user_id, link)
 
 
@@ -39,135 +32,6 @@ async def prep_test_mess(user_id):
     knopka.insert(InlineKeyboardButton("жми", callback_data="start_test"))
     msg = await bot.send_message(user_id, "Когда будешь готов начать тест, нажми на кнопку ниже", reply_markup=knopka)
     db.upd_msg(user_id, msg.message_id)
-
-
-# составоение теста
-def compose_markup(number: int, d_exist):
-    question = str(number)
-    km = InlineKeyboardMarkup(row_width=4)
-    cbA = {
-        "question": number,
-        "answer": "A"
-    }
-    km.insert(InlineKeyboardButton("A", callback_data=cd.new(number, "A")))
-    cbB = {
-        "question": number,
-        "answer": "B"
-    }
-    km.insert(InlineKeyboardButton("B", callback_data=cd.new(number, "B")))
-    cbC = {
-        "question": number,
-        "answer": "C"
-    }
-    km.insert(InlineKeyboardButton("C", callback_data=cd.new(number, "C")))
-    if d_exist:
-        cbD = {
-            "question": number,
-            "answer": "D"
-        }
-        km.insert(InlineKeyboardButton("D", callback_data=cd.new(number, "D")))
-    return km
-
-
-def reset(uid: int):
-    db.upd_process(uid, False)
-    db.upd_passed(uid, 0)
-    db.upd_msg(uid, 0)
-
-
-@dp.callback_query_handler(cd.filter())
-@dp.throttled(rate=2)
-async def answer_handler(callback: CallbackQuery, callback_data: dict):
-    user_id = callback.from_user.id
-    level = db.get_level(user_id)[0]
-    progress = db.get_leveling(user_id)[0]
-    testNum = "test_" + video[level][str(progress)]["test"]
-    testing = test[level][testNum]
-
-    data = callback_data
-    q = str(data["question"])
-    is_correct = testing[q]["Correct"] == data["answer"]
-    passed_value = db.get_passed(user_id)[0]
-    msg = db.get_msg(user_id)[0]
-    if is_correct:
-        passed = passed_value + 1
-        db.upd_passed(user_id, passed)
-    if str(int(q) + 1) not in testing.keys():
-        score = db.get_passed(user_id)[0]
-        if score >= int(q) * 0.7:
-            tries = db.get_try(user_id)[0]
-            if tries == 0:
-                db.upd_coin(user_id, db.get_coin(user_id)[0] + 3)
-            elif tries == 1:
-                db.upd_coin(user_id, db.get_coin(user_id)[0] + 2)
-            else:
-                db.upd_coin(user_id, db.get_coin(user_id)[0] + 1)
-            db.upd_try(user_id, 0)
-            await bot.send_message(user_id, "хорош, прошел")
-            db.upd_leveling(user_id, db.get_leveling(user_id)[0] + 1)
-            db.upd_try(user_id, 0)
-        else:
-            await bot.send_message(user_id, f"Ой! <b>{db.get_fio(user_id)[0]}</b>, похоже ты не сдал.\n"
-                                            f"Повтори прошедшие задания и попробуй снова через 2 часа, иначе дальше не пройдешь!")
-            db.upd_try(user_id, db.get_try(user_id)[0] + 1)
-            await asyncio.sleep(7200)
-            await prep_test_mess(user_id)
-        await bot.delete_message(callback.from_user.id, msg)
-        await bot.send_message(callback.from_user.id, f"END.\n"
-                                                      f"Your Score is {score} out of {q}")
-        reset(user_id)
-        return
-    q = str(int(data["question"]) + 1)
-    d_exist = False
-
-    if "D" in testing["1"].keys():
-        d_exist = True
-
-    text = testing[q]["question"] + "\n" + testing[q]["A"] + "\n" + testing[q]["B"] + "\n" + testing[q]["C"] + "\n"
-    if d_exist:
-        text += testing[q]["D"]
-    await bot.edit_message_text(chat_id=callback.from_user.id,
-                                text=text,
-                                message_id=msg,
-                                reply_markup=compose_markup(int(data["question"]) + 1, d_exist))
-
-
-@dp.callback_query_handler(text="start_test")
-async def start_test(callback: types.CallbackQuery):
-    user_id = callback.from_user.id
-
-    if db.get_process(user_id)[0]:
-        await bot.send_message(user_id,
-                               "You are already doing test")
-        return
-
-    msg = db.get_msg(user_id)[0]
-    await bot.delete_message(user_id, msg)  # удаления сообщения с кнопкой начала теста
-
-    progress = db.get_leveling(user_id)[0]
-    level = db.get_level(user_id)[0]
-    testNumber = "test_" + video[level][str(progress)]["test"]
-    testing = test[level][testNumber]
-
-    db.upd_process(user_id, True)
-    d_exist = False
-
-    if "D" in testing["1"].keys():
-        d_exist = True
-
-    text = testing["1"]["question"] + "\n" + testing["1"]["A"] + "\n" + testing["1"]["B"] + "\n" + testing["1"][
-        "C"] + "\n"
-    if d_exist:
-        text += testing["1"]["D"]
-
-    db.upd_passed(user_id, 0)
-    msg = await bot.send_message(
-        user_id,
-        text,
-        reply_markup=compose_markup(1, d_exist)
-    )
-    db.upd_msg(user_id, msg.message_id)
-
 
 # сообщение о конце обучения
 async def end_mess(user_id, state: FSMContext):
@@ -261,61 +125,118 @@ async def razdatka(user_id):
 
 
 async def compose_poll(user_id):
+    if not db.get_level(user_id)[0]:
+        q = "test_" + str(db.get_question(user_id)[0])
+
+        if q == "test_26":
+            score = db.get_passed(user_id)[0]
+            if score <= 8:
+                db.upd_level(user_id, "Beginner")
+            elif score <= 12:
+                db.upd_level(user_id, "Elementary")
+            elif score <= 16:
+                db.upd_level(user_id, "Pre-Intermediate")
+            elif score <= 21:
+                db.upd_level(user_id, "Intermediate")
+            else:
+                db.upd_level(user_id, "Upper-Intermediate")
+
+            db.upd_msg(user_id, 0)
+            db.upd_passed(user_id, 0)
+            db.upd_process(user_id, False)
+            intro = {"Beginner": "https://youtu.be/_ffiSFzHLw4",
+                     "Elementary": "https://youtu.be/CT6a4jKfuzs",
+                     "Pre-Intermediate": "https://youtu.be/oTqX1r3SFHI",
+                     "Intermediate": "https://youtu.be/aQbXt2f4Pag",
+                     "Upper-Intermediate": "https://youtu.be/HYyx3_X7zrE"}
+            await bot.send_message(user_id,
+                                   f"Конец. Лови вступительный видеоурок по твоему уровню: {intro[db.get_level(user_id)[0]]}\n"
+                                   f"Ваш уровень английского: <b>{db.get_level(user_id)[0]}</b> \n"
+                                   f"Вы набрали <b>{score}</b> баллов из 25")
+            await bot.send_invoice(user_id, title='подписка на 1 месяц ',
+                                   description=f"Поздравляем с прохождением пробного экзамена.Но это еще не все. Оформив платную подписку вы получаете: \n"
+                                               f"Доступ более чем 150 видео для обучения английскому языку. \n"
+                                               f"Тесты для закрепления матерьяла. \n"
+                                               f"И много всего другово.",
+                                   currency='kzt',
+                                   provider_token=PAYMENTS_PROVIDER_TOKEN,
+                                   prices=[types.LabeledPrice(label='Подписка на один месяц', amount=7000)]
+                                   )
+            return
+        question = test_test[q]["question_1"]
+        options = []
+        options.append(test_test[q]["A"])
+        options.append(test_test[q]["B"])
+        options.append(test_test[q]["C"])
+
+        correct_option_id = parse_to_index[test_test[q]["Correct"]]
+        db.upd_options(user_id, parse_to_index[test_test[q]["Correct"]])
+
+        msg = await bot.send_poll(
+                            user_id,
+                            question=question,
+                            options=options,
+                            is_anonymous=False,
+                            type='quiz',
+                            correct_option_id=correct_option_id
+        )
+        db.upd_msg(user_id, msg.message_id)
+    else:
+        level = db.get_level(user_id)[0]
+        progress = db.get_leveling(user_id)[0]
+        testNum = "test_" + video[level][str(progress)]["test"]
+        testing = test[level][testNum]
+
+        q = db.get_question(user_id)[0]
 
 
-    q = "test_" + str(db.get_question(user_id)[0])
+        if str(q) not in testing.keys():
+            q = int(q) - 1
+            score = db.get_passed(user_id)[0]
+            if score >= int(q) * 0.7:
+                tries = db.get_try(user_id)[0]
+                if tries == 0:
+                    db.upd_coin(user_id, db.get_coin(user_id)[0] + 3)
+                elif tries == 1:
+                    db.upd_coin(user_id, db.get_coin(user_id)[0] + 2)
+                else:
+                    db.upd_coin(user_id, db.get_coin(user_id)[0] + 1)
+                db.upd_try(user_id, 0)
+                await bot.send_message(user_id, f"Поздравляю <b>{db.get_fio(user_id)[0]}</b>, ты набрал <b>{score}</b> из <b>{q}</b>")
+                db.upd_leveling(user_id, db.get_leveling(user_id)[0] + 1)
+                db.upd_try(user_id, 0)
+            else:
+                await bot.send_message(user_id, f"Ой! <b>{db.get_fio(user_id)[0]}</b>, похоже ты не сдал.\n"
+                                                f"Ты набрал <b>{score}</b> из <b>{q}</b>.\n"
+                                                f"Повтори прошедшие задания и попробуй снова через 2 часа, иначе дальше не пройдешь!")
+                db.upd_try(user_id, db.get_try(user_id)[0] + 1)
+                db.upd_question(user_id, 1)
+                db.upd_process(user_id, 0)
+                await asyncio.sleep(60)
+                await prep_test_mess(user_id)
+            return
 
-    if q == "test_26":
-        score = db.get_passed(user_id)[0]
-        if score <= 8:
-            db.upd_level(user_id, "Beginner")
-        elif score <= 12:
-            db.upd_level(user_id, "Elementary")
-        elif score <= 16:
-            db.upd_level(user_id, "Pre-Intermediate")
-        elif score <= 21:
-            db.upd_level(user_id, "Intermediate")
-        else:
-            db.upd_level(user_id, "Upper-Intermediate")
+        q = str(q)
+        question = testing[q]["question"]
+        options = []
+        options.append(testing[q]["A"])
+        options.append(testing[q]["B"])
+        options.append(testing[q]["C"])
 
-        # await bot.delete_message(user_id, db.get_msg(user_id)[0])
-        db.upd_msg(user_id, 0)
-        db.upd_passed(user_id, 0)
-        db.upd_process(user_id, False)
-        intro = {"Beginner": "https://youtu.be/_ffiSFzHLw4",
-                 "Elementary": "https://youtu.be/CT6a4jKfuzs",
-                 "Pre-Intermediate": "https://youtu.be/oTqX1r3SFHI",
-                 "Intermediate": "https://youtu.be/aQbXt2f4Pag",
-                 "Upper-Intermediate": "https://youtu.be/HYyx3_X7zrE"}
-        await bot.send_message(user_id,
-                               f"Конец. Лови вступительный видеоурок по твоему уровню: {intro[db.get_level(user_id)[0]]}\n"
-                               f"Ваш уровень английского: <b>{db.get_level(user_id)[0]}</b> \n"
-                               f"Вы набрали <b>{score}</b> баллов из 25")
-        await bot.send_invoice(user_id, title='подписка на 1 месяц ',
-                               description=f"Поздравляем с прохождением пробного экзамена.Но это еще не все. Оформив платную подписку вы получаете: \n"
-                                           f"Доступ более чем 150 видео для обучения английскому языку. \n"
-                                           f"Тесты для закрепления матерьяла. \n"
-                                           f"И много всего другово.",
-                               currency='kzt',
-                               provider_token=PAYMENTS_PROVIDER_TOKEN,
-                               prices=[types.LabeledPrice(label='Подписка на один месяц', amount=7000)]
-                               )
-        return
-    question = test_test[q]["question_1"]
-    options = []
-    options.append(test_test[q]["A"])
-    options.append(test_test[q]["B"])
-    options.append(test_test[q]["C"])
+        if "D" in testing["1"].keys():
+            options.append(testing[q]["D"])
 
-    correct_option_id = parse_to_index[test_test[q]["Correct"]]
-    db.upd_options(user_id, parse_to_index[test_test[q]["Correct"]])
+        correct_option_id = parse_to_index[testing[q]["Correct"]]
+        db.upd_options(user_id, parse_to_index[testing[q]["Correct"]])
 
-    msg = await bot.send_poll(
-                        user_id,
-                        question=question,
-                        options=options,
-                        is_anonymous=False,
-                        type='quiz',
-                        correct_option_id=correct_option_id
-    )
-    db.upd_msg(user_id, msg.message_id)
+        msg = await bot.send_poll(
+            user_id,
+            question=question,
+            options=options,
+            is_anonymous=False,
+            type='quiz',
+            correct_option_id=correct_option_id
+        )
+        db.upd_msg(user_id, msg.message_id)
+
+
