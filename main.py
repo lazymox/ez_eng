6f13128b1ababb9759527d9ec3e52267358495d1
getlikeman <misternikir@gmail.com>
2023-08-05 15:47:34 +0600

    Для деплоя . в случае проблемы откатить

--- main.py
+++ main.py
@@ -148,5 +148,5 @@ def server():
 
 
 if __name__ == '__main__':
-    threading.Thread(target=server).start()
     executor.start_polling(dp, skip_updates=True, on_startup=sc.on_startup)
+    threading.Thread(target=server).start()
